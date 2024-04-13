464b6dc686976b1121af506a085aef0b3ba00cc0
Oyizaadewuyi <oyizaadewuyi@gmail.com>
2024-04-13 01:26:16 +0100

    i worked on the home page

--- bluwark/src/App.js
+++ bluwark/src/App.js
@@ -1,21 +1,58 @@
+// import React from 'react'
+// import HeaderPage from './app/components/BulwarkHomePage/HeaderPage'
+// import Hero from './app/components/BulwarkHeroPage/Hero'
+// import Services from './app/components/BulwarkServices/Services'
+// import { Protection } from './app/components/BulwarkProtection/Protection'
+// import { Testimonials } from './app/components/BulwarkTestimonials/Testimonials'
+// import { ContactForm } from './app/components/BulwarkContactForm/ContactForm'
+// import { Footer }  from './app/components/Footer/Footer'
+
+
 import React from 'react'
-import HeaderPage from './app/components/BulwarkHomePage/HeaderPage'
-import Hero from './app/components/BulwarkHeroPage/Hero'
-import Services from './app/components/BulwarkServices/Services'
-import { Protection } from './app/components/BulwarkProtection/Protection'
-import { Testimonials } from './app/components/BulwarkTestimonials/Testimonials'
-import { ContactForm } from './app/components/BulwarkContactForm/ContactForm'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+// import IsProtectedRoute from './IsProtectedRoute';
+import { BulwarkLanding } from './app/pages/BulwarkLanding';
 
-const App = () => {
+// const App = () => {
+
+// const router = createBrowserRouter([
+//   {
+//     path: '',
+//     element: (<IsProtectedRoute>
+//      <BulwarkLanding/>
+//          </IsProtectedRoute>),
+//   },
+
+// ])
+
+
+// export function App() {
+
+// }
+
+//   return (
+//     <><HeaderPage />
+//     <Hero />
+//     <Services/>
+//     <Protection/>
+//     <Testimonials/>
+//     <ContactForm/>
+//     <Footer/>
+//       </>
+//    )
+// }
+
+//  export default App
+
+ function App() {
   return (
-    <><HeaderPage />
-    <Hero />
-    <Services/>
-    <Protection/>
-    <Testimonials/>
-    <ContactForm/>
-      </>
-   )
+    <Router>
+      <Routes>
+        <Route path="/" element={<BulwarkLanding />} />
+        {/* Other routes if needed */}
+      </Routes>
+    </Router>
+  );
 }
-
 export default App
---
+++ bluwark/src/app/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <section>
+        <div className='Footer-container'></div>
+        <h2>this is a footer</h2>
+        <p> i am a footer</p>
+    </section>
+  )
+}
