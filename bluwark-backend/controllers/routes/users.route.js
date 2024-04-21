270f2042e821652e1a01f2227e9ada628142ffe1
Mutiat Bashua <mutiatbashua@gmail.com>
2024-04-22 00:05:23 +0100

    update json

---
+++ bluwark-backend/controllers/routes/users.route.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+	createUser,
+	loginUser,
+	updateUser,
+	deleteUser,
+	findUser,
+	findAllUsers,
+} = require("../../src/controllers/users.controllers");
+
+// Create a user
+router.post("/signup", createUser);
+
+// Login a user
+router.post("/signin", loginUser);
+
+// Get a user by ID
+router.get("/users/:id", findUser);
+
+// Update a user by ID
+router.patch("/users/:id", updateUser);
+
+// Delete a user by ID
+router.delete("/users/:id", deleteUser);
+
+// Get all users
+router.get("/users", findAllUsers);
+
+module.exports = router;
